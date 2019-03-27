a2ffcaff4cac4f85febf8b1e007eed162f4ca6d3
Spie1er <berulava@gmail.com>
2019-03-27 19:52:32 +0400

    v1.0

---
+++ precache-manifest.9a1d989e77465ad7c964bf9644d42fa5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "04eca4e43f12ba030f126aa52f86c8ef",
+    "url": "/jedi/static/media/14.04eca4e4.png"
+  },
+  {
+    "revision": "d54ab6d8b888715184e7",
+    "url": "/jedi/static/css/main.1fb4a0a0.chunk.css"
+  },
+  {
+    "revision": "fb36fc6e4a1d84c7f416",
+    "url": "/jedi/static/js/runtime~main.a2dfc47d.js"
+  },
+  {
+    "revision": "d8ab6dea99cc4600b2d55d84a0609de2",
+    "url": "/jedi/static/media/Starjhol.d8ab6dea.woff"
+  },
+  {
+    "revision": "cdbfcc5e11fdcb3ab19f",
+    "url": "/jedi/static/js/2.a07e6a3a.chunk.js"
+  },
+  {
+    "revision": "3d5819ede2e7a278f2eaac73cdbef260",
+    "url": "/jedi/static/media/1.3d5819ed.png"
+  },
+  {
+    "revision": "d2bef538a274fb2b52cf44545c3011e5",
+    "url": "/jedi/static/media/10.d2bef538.png"
+  },
+  {
+    "revision": "209966869706fabb1aa86fa2550ab712",
+    "url": "/jedi/static/media/11.20996686.png"
+  },
+  {
+    "revision": "c45ab0ff41202b2dcf1c4583ffb2aef0",
+    "url": "/jedi/static/media/12.c45ab0ff.png"
+  },
+  {
+    "revision": "7f186ed6f8a1baf3ca664b203de1b65f",
+    "url": "/jedi/static/media/13.7f186ed6.png"
+  },
+  {
+    "revision": "d54ab6d8b888715184e7",
+    "url": "/jedi/static/js/main.3915916e.chunk.js"
+  },
+  {
+    "revision": "e3461837b8639378dac503156f24d89d",
+    "url": "/jedi/static/media/2.e3461837.png"
+  },
+  {
+    "revision": "e3f01d33e65686bc1c3da3f0fa48fae1",
+    "url": "/jedi/static/media/3.e3f01d33.png"
+  },
+  {
+    "revision": "cfbece0fb6fc92f84056a07fdf4d927b",
+    "url": "/jedi/static/media/4.cfbece0f.png"
+  },
+  {
+    "revision": "85624582f79e6c00f1c5fb1e9a853687",
+    "url": "/jedi/static/media/5.85624582.png"
+  },
+  {
+    "revision": "5a84e4a30d279b019793a1d84ebc10c3",
+    "url": "/jedi/static/media/6.5a84e4a3.png"
+  },
+  {
+    "revision": "3d61cc1de814c679a7769cbee4b9f349",
+    "url": "/jedi/static/media/7.3d61cc1d.png"
+  },
+  {
+    "revision": "caf49aed655a4b404247ca775ee8a87e",
+    "url": "/jedi/static/media/8.caf49aed.png"
+  },
+  {
+    "revision": "a80b7efdc83ed26f7825eb5b2c65e56d",
+    "url": "/jedi/static/media/9.a80b7efd.png"
+  },
+  {
+    "revision": "cdbfcc5e11fdcb3ab19f",
+    "url": "/jedi/static/css/2.0c7716c5.chunk.css"
+  },
+  {
+    "revision": "1e20f1dd19767114af81a73a4dc4184d",
+    "url": "/jedi/index.html"
+  }
+];
